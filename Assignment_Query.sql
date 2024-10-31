6ffc16b6693aa3153ac94d7e459e4dd0f29cb969
Phan Son <minikie456@gmail.com>
2024-10-31 23:59:11 +0700

    Finish creating all entities and connect it together.

--- Assignment_Query.sql
+++ Assignment_Query.sql
@@ -52,6 +52,81 @@ GO
 ---------------------------------- OTHER ENTITY -----------------------------
 */
 CREATE TABLE [Order](
-	
+	oID int ,
+	cusID int,
+	emID int, 
+	oStatus nvarchar(15),
+	oDate date,
+	oTotalAmount int
+	PRIMARY KEY (oID),
+	FOREIGN KEY (cusID) REFERENCES Customer(cusID),
+	FOREIGN KEY (emID) REFERENCES Employee(emID)
 )
 GO
+
+
+
+CREATE TABLE OrderDetail(
+	oID int,
+	proID int, 
+	oDetailID int,
+	oDetailTotalPrice int,
+	oDetailQuantity int,
+	PRIMARY KEY (oID, oDetailID),
+	FOREIGN KEY (oID) REFERENCES [Order](oID),
+	FOREIGN KEY (proID) REFERENCES [Product](proID)
+
+)
+GO
+
+CREATE TABLE Ingredient(
+	inID int PRIMARY KEY,
+	supID int,
+	wareID int,
+	inName nvarchar(50),
+	inStockQuantity int,
+	FOREIGN KEY (supID) REFERENCES [Supplier](supID),
+	FOREIGN KEY (wareID) REFERENCES [Warehouse](wareID)
+)
+GO
+
+CREATE TABLE ProductIngredients(
+	proID int,
+	inID int,
+	ammountOfIngredients int,
+	dateAdded date,
+	updateDate date,
+	PRIMARY KEY (proID, inID),
+	FOREIGN KEY (inID) REFERENCES [Ingredient](inID),
+	FOREIGN KEY (proID) REFERENCES [Product](proID)
+)
+GO
+
+CREATE TABLE Stores(
+	wareID int,
+	proID int, 
+	inventoryQuantity int,
+	dateOfEntry date,
+	dateOfIssue date,
+	PRIMARY KEY (wareID, proID),
+	FOREIGN KEY (wareID) REFERENCES [Warehouse](wareID),
+	FOREIGN KEY (proID) REFERENCES [Product](proID)
+)
+GO
+/*
+---------------------------------- DROP TABLE SITE -----------------------------
+*/
+DROP TABLE Customer
+DROP TABLE Employee
+DROP TABLE [Product]
+DROP TABLE Supplier
+DROP TABLE Warehouse
+
+DROP TABLE [Order]
+DROP TABLE [OrderDetail]
+DROP TABLE Ingredient
+DROP TABLE ProductIngredients
+DROP TABLE Stores
+/*
+---------------------------------- DROP TABLE SITE -----------------------------
+*/
